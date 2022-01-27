c02900678d318549af18c1f5275e37653a1abc6c
JangYeonji <yunji0220@naver.com>
2022-01-27 20:26:18 +0900

    kakao login

--- src/main/java/com/kumbh/mimo/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/kumbh/mimo/config/auth/dto/OAuthAttributes.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 @Getter
 public class OAuthAttributes {
-    private Map<String, Object> attributes;
+    private Map<String, Object> attributes;     // OAuth2 반환하는 유저 정보 Map
     private String nameAttributeKey;
     private String name;
     private String email;
@@ -25,6 +25,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {         //of 의미 있음...
+        if("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
+        }
         if("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
         }
@@ -50,6 +53,21 @@ public class OAuthAttributes {
                 .email((String) response.get("email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        // kakao는 kakao_account에 유저정보가 있다. (email)
+        Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+        // kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
+        Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .name((String) kakaoProfile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String) kakaoProfile.get("profile_image_url"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }

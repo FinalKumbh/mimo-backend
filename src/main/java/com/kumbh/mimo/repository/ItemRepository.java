6d1570671f7c3060542c787b0f7cc5a029f897b9
JangYeonji <yunji0220@naver.com>
2022-02-03 17:50:22 +0900

    Item Repository

---
+++ src/main/java/com/kumbh/mimo/repository/ItemRepository.java
@@ -0,0 +1,17 @@
+package com.kumbh.mimo.repository;
+
+import com.kumbh.mimo.domain.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item,Long> {
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByPriceLessThan(Integer price);
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+
+}

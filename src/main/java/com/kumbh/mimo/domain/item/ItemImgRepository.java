3d64637bf618558dfa2321512299f1f2f4d106e1
Chaehoon <chaehoon.gwak@gmail.com>
2022-03-02 18:24:09 +0900

    test getAllItems 2

--- src/main/java/com/kumbh/mimo/domain/item/ItemImgRepository.java
+++ src/main/java/com/kumbh/mimo/domain/item/ItemImgRepository.java
@@ -10,11 +10,10 @@ public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
-    @Query(value = "select i.item_id as itemId, i.item_name as itemName, i.item_detail as itemDetail, i.skintype, i.skintone, ii.img_url as imgUrl" +
-            " from item_img ii" +
-            " join item i" +
-            " where i.item_id = ii.item_id;", nativeQuery = true
+    @Query("select new com.kumbh.mimo.dto.item.ItemListDto(i.id, i.itemName, i.itemDetail, i.skintype, i.skintone, ii.imgUrl) " +
+            "from ItemImg ii " +
+            "join Item i " +
+            "where i.id = ii.item.id"
     )
     List<ItemListDto> getAllItems();
-
 }
